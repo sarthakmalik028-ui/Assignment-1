fc932017264a3ba714db5f3e549c88d2744d9429
sarthakmalik028-ui <sarthakmalik028@gmail.com>
2026-02-19 02:38:29 +0530

    initial commit

---
+++ ASSIGNMENT 1 Task-1.py
@@ -0,0 +1,14 @@
+print("Enter two numbers")
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+Add= a + b
+Sub = (a-b)
+Mul = a*b
+Div= a/b
+
+print("Addition:", Add)
+print("Substraction:",Sub )
+print("Multiplication:", Mul)
+print("Division:", Div)
+
---
+++ ASSIGNMENT 1 Task-2.py
@@ -0,0 +1,5 @@
+First_Name = input("Enter your first name   : ")
+Last_name = input("Enter your last name: ")
+
+print("Hello, "+First_Name+" "+Last_name+"! Welcome to the Python program.")
+
